cd6b219d1fffe9b7b3adc08d1c811302a100ba98
lost-melody <lost__melody@163.com>
2025-03-25 13:34:21 +0800

    move bind_option to utils

--- modules/preferences.py
+++ modules/preferences.py
@@ -1,10 +1,9 @@
-from typing import Callable
 from gi.repository import Adw, Gio, GObject, Gtk
 from ignis.widgets import Widget
 from ignis.options import options
-from ignis.options_manager import OptionsGroup
 from .constants import WindowName
 from .template import gtk_template, gtk_template_callback, gtk_template_child
+from .utils import bind_option
 
 
 class Preferences(Widget.RegularWindow):
@@ -28,15 +27,15 @@ class Preferences(Widget.RegularWindow):
 
             if options is not None:
                 if options.notifications is not None:
-                    self.__bind_option(options.notifications, "dnd", self.dnd, "active")
-                    self.__bind_option(
+                    bind_option(options.notifications, "dnd", self.dnd, "active")
+                    bind_option(
                         options.notifications,
                         "popup_timeout",
                         self.popup_timeout,
                         "value",
                         transform_from=lambda f: round(f),
                     )
-                    self.__bind_option(
+                    bind_option(
                         options.notifications,
                         "max_popups_count",
                         self.max_popups,
@@ -45,56 +44,17 @@ class Preferences(Widget.RegularWindow):
                     )
 
                 if options.recorder is not None:
-                    self.__bind_option(
-                        options.recorder, "bitrate", self.bitrate, "value", transform_from=lambda f: round(f)
-                    )
-                    self.__bind_option(options.recorder, "default_filename", self.recorder_filename, "text")
+                    bind_option(options.recorder, "bitrate", self.bitrate, "value", transform_from=lambda f: round(f))
+                    bind_option(options.recorder, "default_filename", self.recorder_filename, "text")
 
                 if options.wallpaper is not None:
-                    self.__bind_option(
+                    bind_option(
                         options.wallpaper,
                         "wallpaper_path",
                         self.wallpaper_path,
                         "subtitle",
                         flags=GObject.BindingFlags.DEFAULT,
                     )
-
-        def __bind_option(
-            self,
-            group: OptionsGroup,
-            option: str,
-            target: GObject.Object,
-            target_property: str,
-            flags: GObject.BindingFlags = GObject.BindingFlags.BIDIRECTIONAL,
-            transform_to: Callable | None = None,
-            transform_from: Callable | None = None,
-        ):
-            binding = group.bind(option, transform_to)
-            source: GObject.Object = binding.target
-            source_property: str = binding.target_properties[0]
-
-            # target.property = transform_to(group.option)
-            def on_option_changed(*_):
-                value = source.get_property(source_property.replace("-", "_"))
-                if transform_to:
-                    value = transform_to(value)
-                if target.get_property(target_property) != value:
-                    target.set_property(target_property, value)
-
-            source.connect(f"notify::{source_property}", on_option_changed)
-            on_option_changed()
-
-            if flags | GObject.BindingFlags.BIDIRECTIONAL == flags:
-
-                # group.option = transform_from(target.property)
-                def on_option_set(*_):
-                    value = target.get_property(target_property)
-                    if transform_from:
-                        value = transform_from(value)
-                    if getattr(group, option) != value:
-                        setattr(group, option, value)
-
-                target.connect(f"notify::{target_property}", on_option_set)
 
         @gtk_template_callback
         def on_wallpaper_select_clicked(self, *_):
--- modules/utils.py
+++ modules/utils.py
@@ -1,9 +1,10 @@
 import json
 from asyncio import create_task
 from typing import Any, Callable
-from gi.repository import Gdk, Gtk
+from gi.repository import Gdk, GObject, Gtk
 from ignis.widgets import Widget
 from ignis.services.niri import NiriService
+from ignis.options_manager import OptionsGroup
 from ignis.utils.shell import exec_sh_async
 from ignis.utils.monitor import get_monitor
 
@@ -39,6 +40,46 @@ def connect_window(widget: Gtk.Widget, signal: str, callback: Callable[..., Any]
             window.connect(signal, callback)
 
     widget.connect("realize", on_realize)
+
+
+def connect_option(group: OptionsGroup, option: str, callback: Callable):
+    binding = group.bind(option)
+    source: GObject.Object = binding.target
+    source_property: str = binding.target_properties[0]
+    source.connect(f"notify::{source_property.replace("-", "_")}", callback)
+
+
+def bind_option(
+    group: OptionsGroup,
+    option: str,
+    target: GObject.Object,
+    target_property: str,
+    flags: GObject.BindingFlags = GObject.BindingFlags.BIDIRECTIONAL,
+    transform_to: Callable | None = None,
+    transform_from: Callable | None = None,
+):
+    # target.property = transform_to(group.option)
+    def on_option_changed(*_):
+        value = getattr(group, option)
+        if transform_to:
+            value = transform_to(value)
+        if target.get_property(target_property) != value:
+            target.set_property(target_property, value)
+
+    connect_option(group, option, on_option_changed)
+    on_option_changed()
+
+    if flags | GObject.BindingFlags.BIDIRECTIONAL == flags:
+
+        # group.option = transform_from(target.property)
+        def on_option_set(*_):
+            value = target.get_property(target_property)
+            if transform_from:
+                value = transform_from(value)
+            if getattr(group, option) != value:
+                setattr(group, option, value)
+
+        target.connect(f"notify::{target_property}", on_option_set)
 
 
 def set_on_click[Widget: Gtk.Widget](
