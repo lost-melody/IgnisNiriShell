3463c83f7a4962e4a6c80b3c21fa33769d43b900
lost-melody <lost__melody@163.com>
2025-04-03 20:27:46 +0800

    remove unused window name

--- modules/constants.py
+++ modules/constants.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class WindowName(Enum):
     top_bar = "ignis-topbar"
-    bottom_bar = "ignis-bottombar"
     app_launcher = "ignis-applauncher"
     app_dock = "ignis-appdock"
     control_center = "ignis-controlcenter"
