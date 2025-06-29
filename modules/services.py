b61f24afa411425078cec9ec4450a7f77217cfd9
lost-melody <lost__melody@163.com>
2025-06-29 15:51:36 +0800

    feat (fcitx): implement kimpanel dbus service

--- modules/services.py
+++ modules/services.py
@@ -1,12 +1,13 @@
-import asyncio
+import dataclasses
+import enum
 import os
 from typing import Any
-from gi.repository import Gio, GLib
+from gi.repository import GLib
 from loguru import logger
 from ignis.base_service import BaseService
-from ignis.dbus import DBusProxy
-from ignis.gobject import IgnisProperty
-from ignis.utils import file_monitor, load_interface_xml, Poll, thread
+from ignis.dbus import DBusProxy, DBusService
+from ignis.gobject import IgnisGObject, IgnisProperty
+from ignis.utils import load_interface_xml, Poll, thread
 
 
 try:
@@ -86,40 +87,260 @@ class CpuLoadService(BaseService):
 class FcitxStateService(BaseService):
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
+    class KIMPanel(IgnisGObject):
+        class SignalName(enum.Enum):
+            Enable = "Enable"
+            ExecDialog = "ExecDialog"
+            ExecMenu = "ExecMenu"
+            RegisterProperties = "RegisterProperties"
+            RemoveProperty = "RemoveProperty"
+            ShowAux = "ShowAux"
+            ShowLookupTable = "ShowLookupTable"
+            ShowPreedit = "ShowPreedit"
+            UpdateAux = "UpdateAux"
+            UpdateLookupTableCursor = "UpdateLookupTableCursor"
+            UpdatePreeditCaret = "UpdatePreeditCaret"
+            UpdatePreeditText = "UpdatePreeditText"
+            UpdateProperty = "UpdateProperty"
+            UpdateScreen = "UpdateScreen"
+            UpdateSpotLocation = "UpdateSpotLocation"
+
+        @dataclasses.dataclass
+        class Property:
+            key: str = ""
+            label: str = ""
+            icon: str = ""
+            text: str = ""
+            hint: list[str] = dataclasses.field(default_factory=list)
+
+        @dataclasses.dataclass
+        class Rect:
+            x: int = 0
+            y: int = 0
+            w: int = 0
+            h: int = 0
+
+        def __init__(self):
+            super().__init__()
+
+            self._enabled = False
+            self._show_aux = False
+            self._show_lookup = False
+            self._show_preedit = False
+            self._aux = ""
+            self._preedit = ""
+            self._fcitx_im = self.Property(key="/Fcitx/im")
+            self._spot = self.Rect()
+
+            self.impanel = DBusService(
+                name="org.kde.impanel",
+                object_path="/org/kde/impanel",
+                info=load_interface_xml(
+                    path=os.path.join(FcitxStateService.current_dir, "dbus", "org.kde.impanel.xml")
+                ),
+                on_name_acquired=self.__on_impanel_acquired,
+            )
+
+            self.impanel2 = DBusService(
+                name="org.kde.impanel",
+                object_path="/org/kde/impanel",
+                info=load_interface_xml(
+                    path=os.path.join(FcitxStateService.current_dir, "dbus", "org.kde.impanel2.xml")
+                ),
+                on_name_acquired=self.__on_impanel2_acquired,
+            )
+            self.__register_methods2(self.impanel2)
+
+            self.proxy = DBusProxy.new(
+                name="org.fcitx.Fcitx5",
+                object_path="/kimpanel",
+                interface_name="org.kde.kimpanel.inputmethod",
+                info=load_interface_xml(
+                    path=os.path.join(FcitxStateService.current_dir, "dbus", "org.kde.impanel.inputmethod.xml")
+                ),
+                bus_type="session",
+            )
+            self.__subscribe_signals(self.proxy)
+
+        @IgnisProperty
+        def enabled(self) -> bool:
+            return self._enabled
+
+        @IgnisProperty
+        def show_aux(self) -> bool:
+            return self._show_aux
+
+        @IgnisProperty
+        def show_lookup(self) -> bool:
+            return self._show_lookup
+
+        @IgnisProperty
+        def show_preedit(self) -> bool:
+            return self._show_preedit
+
+        @IgnisProperty
+        def aux(self) -> str:
+            return self._aux
+
+        @IgnisProperty
+        def preedit(self) -> str:
+            return self._preedit
+
+        @IgnisProperty
+        def fcitx_im(self) -> Property:
+            return self._fcitx_im
+
+        @IgnisProperty
+        def spot(self) -> Rect:
+            return self._spot
+
+        def signal_trigger_property(self, key: str):
+            self.impanel.emit_signal("TriggerProperty", GLib.Variant.new_tuple(GLib.Variant.new_string(key)))
+
+        def signal_exit(self):
+            self.impanel.emit_signal("Exit")
+
+        def signal_reload_config(self):
+            self.impanel.emit_signal("ReloadConfig")
+
+        def signal_configure(self):
+            self.impanel.emit_signal("Configure")
+
+        def __on_impanel_acquired(self, *_):
+            self.impanel.emit_signal("PanelCreated")
+
+        def __on_impanel2_acquired(self, *_):
+            self.impanel.emit_signal("PanelCreated2")
+
+        def __register_methods2(self, dbus: DBusService):
+            dbus.register_dbus_method("SetSpotRect", self.__dbus_set_spot_rect)
+            dbus.register_dbus_method("SetLookupTable", self.__dbus_set_lookup_table)
+
+        def __subscribe_signals(self, proxy: DBusProxy):
+            for signal in self.SignalName:
+                proxy.signal_subscribe(signal.value, self.__on_signal)
+
+        def __dbus_set_spot_rect(self, _, x: int, y: int, w: int, h: int):
+            self._spot = self.Rect(x, y, w, h)
+            self.notify("spot")
+
+        def __dbus_set_lookup_table(
+            self,
+            _,
+            label: list[str],
+            text: list[str],
+            attr: list[str],
+            hasPrev: bool,
+            hasNext: bool,
+            cursor: int,
+            layout: int,
+        ):
+            pass
+
+        def __on_signal(self, _, __, ___, ____, signal: str, param: GLib.Variant):
+            match self.SignalName(signal):
+                case self.SignalName.Enable:
+                    # input method enabled
+                    self._enabled = param.get_child_value(0).get_boolean()
+                    self.notify("enabled")
+                case self.SignalName.ExecMenu:
+                    # show menu: list[str]
+                    pass
+                case self.SignalName.RegisterProperties:
+                    # register properties
+                    pass
+                case self.SignalName.ShowAux:
+                    # show and hide aux tooltip
+                    self._show_aux = param.get_child_value(0).get_boolean()
+                    self.notify("show-aux")
+                case self.SignalName.ShowLookupTable:
+                    # show and hide lookup table
+                    self._show_lookup = param.get_child_value(0).get_boolean()
+                    self.notify("show-lookup")
+                case self.SignalName.ShowPreedit:
+                    # show and hide preedit text
+                    self._show_preedit = param.get_child_value(0).get_boolean()
+                    self.notify("show-preedit")
+                case self.SignalName.UpdateAux:
+                    # update aux tooltip
+                    self._aux = param.get_child_value(0).get_string()
+                    self.notify("aux")
+                case self.SignalName.UpdatePreeditText:
+                    # update preedit text
+                    self._preedit = param.get_child_value(0).get_string()
+                    self.notify("preedit")
+                case self.SignalName.UpdateProperty:
+                    # update property
+                    self._fcitx_im = self.__parse_property(param.get_child_value(0).get_string())
+                    self.notify("fcitx-im")
+                case self.SignalName.UpdateSpotLocation:
+                    # update spot location: x, y
+                    x = param.get_child_value(0).get_int32()
+                    y = param.get_child_value(0).get_int32()
+                    self._spot = self.Rect(x, y, self.spot.w, self.spot.h)
+                    self.notify("spot")
+
+        def __parse_property(self, property: str) -> Property:
+            [key, label, icon, text, hint] = property.split(":")
+            hint = hint.split(",")
+            for h in hint:
+                if h.startswith("label="):
+                    label = h.lstrip("lable=")
+                    icon = ""
+                    break
+            return self.Property(key, label, icon, text, hint)
+
     def __init__(self):
         super().__init__()
 
-        self._is_active: bool = False
-        self._current_input_method: str = ""
-        self._current_schema: str = ""
-        self._is_ascii_mode: str = ""
+        self._label: str = ""
+        self._text: str = ""
+        self._icon: str = ""
 
-        run_dir = os.getenv("XDG_RUNTIME_DIR")
-        if run_dir:
-            fm = file_monitor.FileMonitor(
-                path=os.path.join(run_dir, "fcitx-ignis-signal"), callback=self.__on_fcitx_signal
-            )
-            if isinstance(fm._monitor, Gio.FileMonitor):
-                fm._monitor.set_rate_limit(50)
+        self._kimpanel = self.KIMPanel()
+        self._kimpanel.connect("notify::fcitx-im", self.__on_fcitx_im_changed)
 
     @IgnisProperty
-    def is_active(self) -> bool:
-        return self._is_active
+    def kimpanel(self) -> KIMPanel:
+        return self._kimpanel
 
     @IgnisProperty
-    def current_input_method(self) -> str:
-        return self._current_input_method
+    def label(self) -> str:
+        return self._label
 
     @IgnisProperty
-    def current_schema(self) -> str:
-        return self._current_schema
+    def text(self) -> str:
+        return self._text
 
     @IgnisProperty
-    def is_ascii_mode(self) -> str:
-        return self._is_ascii_mode
+    def icon(self) -> str:
+        return self._icon
 
-    def __on_fcitx_signal(self, *_):
-        asyncio.create_task(self.sync_state_async())
+    async def is_active(self, proxy: DBusProxy | None = None) -> bool:
+        proxy = proxy or await self.__fcitx_proxy()
+        state = await proxy.StateAsync("()")
+        return state[0] == 2
+
+    async def toggle_activate(self):
+        proxy = await self.__fcitx_proxy()
+        if await self.is_active(proxy):
+            await proxy.DeactivateAsync("()")
+        else:
+            await proxy.ActivateAsync("()")
+
+    def __on_fcitx_im_changed(self, *_):
+        prop = self._kimpanel.fcitx_im
+        # "Keyboard - French - French (AZERTY, AFNOR)"
+        text = prop.text.split("-")[-1].strip(" ")
+        if self.label != prop.label:
+            self._label = prop.label
+            self.notify("label")
+        if self.text != text:
+            self._text = text
+            self.notify("text")
+        if self.icon != prop.icon:
+            self._icon = prop.icon
+            self.notify("icon")
 
     async def __fcitx_proxy(self):
         return await DBusProxy.new_async(
